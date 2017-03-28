3958d36d2fb456355c1d4054d26508e4a5dcfb25
WillyWoosley <wdwoosley@gmail.com>
2017-03-28 07:42:43 -0700

    Initial test commit

--- app/src/main/java/com/gamecodeschool/schoolutility/MenubarFragment.java
+++ app/src/main/java/com/gamecodeschool/schoolutility/MenubarFragment.java
@@ -1,5 +1,6 @@
 package com.gamecodeschool.schoolutility;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,22 +8,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by wdwoo on 3/25/2017.
  */
 
 public class MenubarFragment extends Fragment {
+    OnFragmentInteractionListener mListener;
+
+
+    public interface OnFragmentInteractionListener
+    {
+        public void onFragmentInteraction(int position);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View rootView = inflater.inflate(R.layout.menubar_fragment, container, false);
 
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
         //References to bottom action bar options
-        ImageButton newsSwap = (ImageButton) rootView.findViewById(R.id.mainNews);
-        ImageButton contactsSwap = (ImageButton) rootView.findViewById(R.id.mainContacts);
-        ImageButton homeworkSwap = (ImageButton) rootView.findViewById(R.id.mainHomework);
-        ImageButton eventsSwap = (ImageButton) rootView.findViewById(R.id.mainEvents);
+        ImageButton newsSwap = (ImageButton) getView().findViewById(R.id.mainNews);
+        ImageButton contactsSwap = (ImageButton) getView().findViewById(R.id.mainContacts);
+        ImageButton homeworkSwap = (ImageButton) getView().findViewById(R.id.mainHomework);
+        ImageButton eventsSwap = (ImageButton) getView().findViewById(R.id.mainEvents);
 
         //Creates Intents for all buttons
         final Intent homeworkIntent = new Intent(getActivity(), HomeworkActivity.class);
@@ -37,7 +55,19 @@ public class MenubarFragment extends Fragment {
                     }
                 }
         );
-
-        return rootView;
     }
+/*
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+
+        try {
+            mCallback = (OnImageSelectedListener) activity;
+        }
+        catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnImageSelectedListener");
+        }
+    }
+    */
 }
